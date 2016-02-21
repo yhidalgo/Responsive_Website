228df68f766e8c4140cd9c4799d5e95798b5b372
yhidalgo <yhidalgo@asu.edu>
2016-02-21 14:36:05 -0700

    Added simple animals template to index.html

--- 2 Responsive Web Design/Assignment 2 - Data Driven Website/assignment.js
+++ 2 Responsive Web Design/Assignment 2 - Data Driven Website/assignment.js
@@ -10,11 +10,27 @@ function showTemplate(template, data) {
 
 $(document).ready(function(){
    categoryTemplate = Handlebars.compile($("#categoryTemplate").html());
+   animalsTemplate = Handlebars.compile($("#animalsTemplate").html());
    //TODO: animalsTemplate
    
    
    $("#my-js-categoryTab").click(function () {
       showTemplate(categoryTemplate,animals_data);
+      
+      $(".my-js-navigationTab .active").removeClass("active");
+      $("#my-js-categoryTab").addClass("active");
+      
+      $(".my-js-category").click(function () {
+         var currentCategoryIndex = $(this).data("id");
+         currentCategory = animals_data.category[currentCategoryIndex];
+         
+         showTemplate(animalsTemplate,currentCategory);
+         
+         $(".my-js-navigationTab").removeClass("active");
+         $("#my-js-animalsTab").addClass("active");
+         
+         
+      });
    });
    
    $("#my-js-categoryTab").click();
