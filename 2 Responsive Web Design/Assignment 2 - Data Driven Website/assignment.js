3d97c3ee18f543d32d7c48920107433f753b00ce
yhidalgo <yhidalgo@asu.edu>
2016-02-21 13:46:19 -0700

    Fixed category template in index.html and it gets compiled and displayed by javascript code

--- 2 Responsive Web Design/Assignment 2 - Data Driven Website/assignment.js
+++ 2 Responsive Web Design/Assignment 2 - Data Driven Website/assignment.js
@@ -1,5 +1,22 @@
 
 var categoryTemplate, animalsTemplate;
 
+var currentCategory = animals_data.category[0];
 
+function showTemplate(template, data) {
+   var html = template(data);
+   $("#content").html(html);
+}
+
+$(document).ready(function(){
+   categoryTemplate = Handlebars.compile($("#categoryTemplate").html());
+   //TODO: animalsTemplate
+   
+   
+   $("#my-js-categoryTab").click(function () {
+      showTemplate(categoryTemplate,animals_data);
+   });
+   
+   $("#my-js-categoryTab").click();
+});
 
