35ada98bc046fad8ee4e571bc9d725b777407785
yhidalgo <yhidalgo@asu.edu>
2016-02-22 22:52:27 -0700

    Added the cookiesdirective.js script, which provides compliance with the 'Cookies Directive' law passed on 2011 by the European Commission requiring european business websites to obtain consent to use cookies (even if implicit) from users.  http://cookiesdirective.com/

--- 2 Responsive Web Design/Assignment 2 - Data Driven Website/assignment.js
+++ 2 Responsive Web Design/Assignment 2 - Data Driven Website/assignment.js
@@ -79,5 +79,40 @@ $(document).ready(function(){
    });
    
    $("#my-js-categoryTab").click();
+   
+   
+   /*  The cookiesdirective.js script provides compliance with the 'Cookies Directive' law passed on 2011 by the European Commission requiring european business websites to obtain consent to use cookies (even if implicit) from users.  http://cookiesdirective.com/ */
+   function cookieController() {
+      // Load Google Analytics to header
+      $.cookiesDirective.loadScript({
+         uri:'google.js',                 //TODO
+         appendTo: 'header'
+      });
+             
+         // Load My Stats to body (default)
+      $.cookiesDirective.loadScript({
+         uri:'mystats.js'                //TODO
+      });
+   }   
+   
+   $.cookiesDirective({
+      explicitConsent: false, // false allows implied consent; default is true
+      position: 'bottom', // top or bottom of viewport; default is 'top'
+      duration: 15, // display time in seconds; default is 10
+      limit: 0, // limit disclosure appearances, 0 is forever; default is 0
+      message: null, // customize the disclosure message; default is null             //TODO
+      cookieScripts: null, // disclose cookie settings scripts; default is null       //TODO
+      privacyPolicyUri: 'my-privacy-policy.html',     //TODO
+      cookieScripts: 'Google Analytics,My Stats',      //TODO
+      scriptWrapper: cookieController,
+      fontFamily: 'helvetica', // font style for disclosure panel; default 'helvetica'
+      fontColor: '#FFFFFF', // font color for disclosure panel; default '#FFFFFF'
+      fontSize: '10px', // font size for disclosure panel; default '13px'
+      backgroundColor: '#777777', // background color of disclosure panel; default '#000000'
+      backgroundOpacity: '90', // opacity of disclosure panel; default '80'
+      linkColor: '#CA0000' // link color in disclosure panel; default '#CA0000'
+   });
+   /* End of 'Cookies Directive' compliance code */
+   
 });
 
